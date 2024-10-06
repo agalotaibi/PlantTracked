9a3a3bec74dcac965c27f65b89611caba6f51293
agalotaibi <hopes.gmo@gmail.com>
2024-10-06 14:20:16 +0300

    Initial Commit

---
+++ PlantTracked/PlantTrackedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantTrackedApp.swift
+//  PlantTracked
+//
+//  Created by Amaal on 03/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantTrackedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
